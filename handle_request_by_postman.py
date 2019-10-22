2ea70bb1b210dc682733c57567af6d61cea0fa82
lofues <junfuwang@163.com>
2019-10-22 15:42:02 +0800

    10月22日 tornodo参数获取

---
+++ handle_request_by_postman.py
@@ -0,0 +1,125 @@
+
+# tornodo读取客户端提交的访问参数演示
+
+import tornado
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from tornado.options import define, options, parse_config_file
+from tornado.web import Application, RequestHandler, url
+
+# 演示响应函数
+
+#用来响应用户请求
+class IndexHandler(RequestHandler):
+    # 响应以get方式发送的请求
+    def get(self,*args,**kwargs):
+
+        arg = self.get_query_argument('day',None)
+        print('arg-->',arg)
+
+        args = self.get_query_arguments('day')
+        print('args-->',args)
+
+        self.write('<a href="/python">hello python</a>')
+
+    # 响应以post方式发送的请求
+    def post(self,*args,**kwargs):
+        self.write('hello post')
+        # name = self.get_body_argument('name',None)
+        # print(name,email)
+        # self.write(name+email)
+        day = self.get_body_arguments('day')
+        day1 = self.get_query_arguments('day')
+        print('post-day:',day)
+        print('query-day:',day)
+        day2 = self.get_arguments('day')
+        print('get_arguments:',day2)
+        day3 = self.get_argument('day')
+        print('get_argument:',day3)
+        self.write('received')
+
+# 用来响应 /java 请求
+class JavaHandler(RequestHandler):
+    # 重写RequestHandler中的initialize方法
+    # 获取路由列表中动态设置的参数(greeting,info)
+    def initialize(self,greeting,info):
+        self.greeting = greeting
+        self.info = info
+
+    def get(self,*args,**kwargs):
+        # write方法只能接收一个字符串类型的参数
+        self.write('{} {}'.format(self.greeting,self.info))
+
+
+# 用来响应 /python 请求,处理如下路由的请求
+# tornodo 路由列表
+# /python
+# /python/day1
+# /python/day1/basic
+class PythonHandler(RequestHandler):
+    def get(self,*args,**kwargs):
+        self.write('<a href={}>jump to java</a>'.format(self.reverse_url('java_url')))
+        self.write('<br>')
+
+        # 获取命名参数
+        day = kwargs.get('day',None)
+        title = kwargs.get('title',None)
+
+        if day:
+            self.write('day:'+day+'<br>')
+        if title:
+            self.write('title:'+title)
+
+
+
+
+
+# 演示配置文件
+
+# 定义一个变量,用来代表端口号,若配置文件中找不到port,则使用default  multiple 是否为多个元素
+define('port',type=int,default=11111,multiple=False)
+# 再定义一个变量,用来代表连接数据库的连接信息(用户名,密码,端口,数据库名称)
+define('db',multiple=True,default=[])
+
+# 从指定的配置文件中,读取配置文件中的内容
+parse_config_file('configure')
+print(options.db)
+print(options.port)
+
+
+
+
+
+# 演示路由列表
+
+# 创建application对象,进行对若干个服务器的设置
+# 例如,路由列表 静态资源路径 模板路径
+
+
+
+app = Application([
+    ('/', IndexHandler),
+    # 使用反向解析   调用时在类中使用self.reverse_url()
+    url('/java', JavaHandler,{'greeting': '你好','info': '家娃'},name='java_url'),
+    url('/python', PythonHandler,name='python_url'),
+    # 演示位置传参
+    # url('/python/(\w+)', PythonHandler),
+    # url('/python/(\w+)/(\w+)', PythonHandler),
+
+    # 演示命名传参
+    url('/python/(?P<day>\w+)', PythonHandler),
+    url('/python/(?P<day>\w+)/(?P<title>\w+)', PythonHandler),
+])
+
+
+
+
+
+# 演示服务器配置
+
+# 创建服务器程序
+server = HTTPServer(app)
+# 服务器监听某个端口(建议使用10000以上的端口)
+server.listen(options.port)
+# 启动服务器(在当前进程中)
+IOLoop.current().start()
